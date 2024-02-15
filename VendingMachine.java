eebe0a26d39d857c946355fdffe459007070b154
pferhan <hannah.pfersch@quinnipiac.edu>
2024-02-15 15:32:10 -0500

    a large variety of snacks are available for purchase

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
+    private Map<String, Double> snacks;
+
+    public VendingMachine() {
+        snacks = new HashMap<>(); 
+        snacks.put("Doritos",2.0);
+        snacks.put("Potato Chips",2.0);  
+        snacks.put("Pretzels",1.5);  
+        snacks.put("m&ms",2.5);  
+        snacks.put("Goldfish",2.0);  
+        snacks.put("Cheez-its",2.5);  
+        snacks.put("Mint Gum",.5);  
+        snacks.put("Skittles",2.0);  
+        snacks.put("Snickers",1.5);  
+        snacks.put("KitKat",1.5);  
+        snacks.put("Cheetos",2.0);  
+        snacks.put("Sour Patch Kids",1.5);    
+}
+   
+   
+   
     public static void main(String[] args) 
     {
-		System.out.println("This is a vending machine");
+	
 	}
 }
